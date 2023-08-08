6fd6e00730da2f476be58a0ce8271576f0672225
Suleyman Musayev <slmusayev@gmail.com>
2023-08-08 17:23:30 +0500

    fix references in the main file

--- lib/pg_search_multiple_highlight.rb
+++ lib/pg_search_multiple_highlight.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "pg_search"
-require_relative "pg_search_highlight/version"
-require_relative "pg_search_highlight/error"
-require_relative "pg_search_highlight/tsearch"
-require_relative "pg_search_highlight/scope_options"
+require_relative "pg_search_multiple_highlight/version"
+require_relative "pg_search_multiple_highlight/error"
+require_relative "pg_search_multiple_highlight/tsearch"
+require_relative "pg_search_multiple_highlight/scope_options"
