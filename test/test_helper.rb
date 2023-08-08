3570a018bba2690f1c1638ae9bcc6103983569f6
Suleyman Musayev <slmusayev@gmail.com>
2023-08-08 17:21:28 +0500

    update test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "pg_search_multiple_highlight"
+require "bundler/setup"
+
+require "single_cov"
+SingleCov.setup :minitest
 
 require "minitest/autorun"
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require "pg_search_multiple_highlight"
