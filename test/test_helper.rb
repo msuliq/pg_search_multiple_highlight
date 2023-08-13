a998ae1360fbaf569290184b72db02764ba19c3d
Suleyman Musayev <slmusayev@gmail.com>
2023-08-13 15:50:03 +0500

    update test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,3 +9,20 @@ require "minitest/autorun"
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
 require "pg_search_multiple_highlight"
+require_relative "support/with_model"
+
+require "active_record"
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
+
+ActiveRecord::Schema.define do
+  # Define migrations (create_table, add_column, etc.)
+  create_table :with_models do |t|
+    t.string :title
+    t.string :content
+    # ... other columns
+  end
+end
+
+# Set up DatabaseCleaner
+require "database_cleaner"
+DatabaseCleaner.strategy = :transaction
